1f24c6265ab87808eb311011418667d3f42be54b
lLuiz Gustavo <luiz.gustavo@acto.com.br>
2023-08-03 12:55:22 -0300

    projeto finalizado

---
+++ Models/Moeda.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoBalcaoOfertasAPI.Models
+{
+    public class Moeda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Simbolo { get; set; }
+    }
+}
